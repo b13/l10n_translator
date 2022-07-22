d1ebbaa7dc6dba267f7bd481a70d91a2fe2df529
Daniel Goerz <daniel.goerz@b13.com>
2022-07-22 13:41:08 +0200

    [BUGFIX] Bring back XCLASS

---
+++ ext_localconf.php
@@ -0,0 +1,10 @@
+<?php
+
+if (!defined('TYPO3_MODE')) {
+    die('Access denied.');
+}
+
+// XCLASS for enabling reading default language labels from l10n resp. var/labels
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Core\Localization\Parser\XliffParser::class] = [
+    'className' => \B13\L10nTranslator\Localization\Parser\XliffParser::class
+];
